23050dd59759914cf2217ffe96cf67b856154c64
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-07-18 22:40:55 +0100

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,14 @@
 /**
  * main - prints "abcdefghijklmnopqrstuvwxyz"
  *
- * Return: Always 0 
+ * Return: Always 0
+ * print_alphabet: print a to z
  */
 void print_alphabet(void)
 {
 	char ch;
 	ch = 'a';
+
 	while (ch <= 'z')
 	{
 		_putchar(ch);
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,19 @@
 /**
  * main - print a to z 10x
  *
- * Return: Always 0 
+ * Return: Always 0
+ *
+ * print_alphabet_x10: return alphabets from a to z 10x
  */
 void print_alphabet_x10(void)
 {
 	int i = 1;
+
 	while (i <= 10)
 	{
 		char ch;
 		ch = 'a';
+
 		while (ch <= 'z')
 		{
 			_putchar(ch);
