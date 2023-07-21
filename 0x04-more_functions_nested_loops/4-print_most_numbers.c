d9a4fc81d5191cca6fd147b3f3b0210ebad49a2f
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-07-21 09:59:22 +0100

    Fifth task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - it prints numbers from 0 to 9 without 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
