c91c91f2ec435cdc2b0e6c3a0332a52f3b574f0f
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-07-21 08:30:49 +0100

    Third task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: it stores an integer
+ * @b: It also stores an integer
+ *
+ * Return: returns the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
