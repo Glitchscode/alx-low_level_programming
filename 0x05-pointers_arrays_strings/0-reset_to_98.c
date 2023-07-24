66ac03eb5a9fbcf510c18059e2c43b16adbae4d8
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-07-24 14:23:59 +0100

    First and Second task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - It takes a pointer to an int as a parameter and
  * updates the value its points to to 98
- * @*n: a parameter that changes the variable of the main function
+ * @n: a parameter that changes the variable of the main function
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int - It swap the value of two integers
- * @*a: this parameter swaps with b parameter
- * @*b: this parameter swaps with a parameter
+ * @a: this parameter swaps with b parameter
+ * @b: this parameter swaps with a parameter
  */
 void swap_int(int *a, int *b)
 {
