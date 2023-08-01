ccab24ea59bdaa3955377c5dd0ad776df0c5e5c7
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-08-01 16:52:50 +0100

    First task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * memset - Fills the first n bytes of the memory area pointed to by
+ * @s with the constant byte @c.
+ * @s: pointer to the mem area to be filled.
+ * @b: The character to fill the memory area with.
+ * @n: The no. of bytes to be filled.
+ *
+ * Return: A pointer to the filled memarea @s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
