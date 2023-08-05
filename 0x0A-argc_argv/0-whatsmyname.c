401ae80234befea710e9d7d0ebe0b24a75db6972
Glitchscode <sanuthabdulmumin2@gmail.com>
2023-08-05 18:21:02 +0100

    First task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,13 @@
 #include "main.h"
 /**
  * main - prints its name followed by a new line.
- * argc: argument count.
- * argv: argument array.
+ * @argc: argument count.
+ * @argv: argument array.
  *
  * return: Always zero.
  */
 int main (int argc __attribute__((unused)), char *argv[])
 {
 	_puts(argv[0]);
-	_putchar('\n');
 	return (0);
 }
