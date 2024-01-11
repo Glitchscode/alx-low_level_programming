0ee1925b4e59b10ca088e8d0d2f513c255f342eb
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-01-11 23:07:48 +0100

    Updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,39 @@
 #include "main.h"
 /**
- *
- *
+ * divisors - return n and m
+ * @n: integer n
+ * @m: integer m
+ * Return: return n and m
  */
-int is_prime_number(int n)
+int divisors(int n, int m)
 {
-	if (n / n == 1 || n / 1 == n)
+	if (m % n == 0)
 	{
-		return (1);
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
 	}
 	else
 	{
+		return (1);
+	}
+}
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: stores the number
+ * Return: returns 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
 		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
 }
