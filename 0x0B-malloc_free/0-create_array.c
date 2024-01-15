7fc298ca2da323c606fad6cbb4eee561b55370a8
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-01-15 10:32:09 +0100

    Updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
 	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
 	for (n = 0; n <= size; n++)
 	{
 		ar[n] = c;
