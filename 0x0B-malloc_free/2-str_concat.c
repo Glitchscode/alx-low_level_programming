e3c439b6a0c6735a78728ddd00467d17f0acd4f3
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-01-17 00:26:40 +0100

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, len;
+	int i, len, len1;
 	char *c;
 
 	if (s1 == NULL)
@@ -24,7 +24,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		len++;
 	}
-	c = malloc(len * sizeof(char));
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		len1++;
+	}
+	c = malloc((len + len1 + 1) * sizeof(char));
 	if (c == NULL)
 	{
 		free(c);
