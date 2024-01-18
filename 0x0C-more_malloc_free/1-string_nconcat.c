bc6735e249d59e88f878368f8b9d226512b05158
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-01-18 21:43:43 +0100

    Updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
  * string_nconcat - concatenates two strings.
  * @s1: first string.
  * @s2: second string.
@@ -10,43 +23,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, k;
+	int i, j, len;
 	int num;
 	char *c;
 
 	num = n;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
 	if (num < 0)
 		return (NULL);
-	if (num >= j)
-	{
-		num = j;
-	}
-	c = malloc((i + num + 1) * sizeof(*c));
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+	len = _strlen(s1) + num + 1;
+	c = malloc(len * sizeof(*c));
 	if (c == NULL)
 	{
 		free(c);
 		return (NULL);
 	}
-	for (k = 0; k < i; k++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		c[k] = s1[k];
+		c[i] = s1[i];
 	}
-	for (j = 0; j <= num; k++, j++)
+	for (j = 0; j < num; j++)
 	{
-		c[k] = s2[j];
+		c[i + j] = s2[j];
 	}
-	c[k] = '\0';
+	c[i + j] = '\0';
 	return (c);
 }
